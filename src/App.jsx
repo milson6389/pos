2b8bcd7c29d8431a159b49a89417f5cf2aad21d6
milson6389 <audie.milson38@gmail.com>
2024-09-25 22:05:49 +0700

    Feat: Init POS App Project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="my-5">
+      <h1 className="text-2xl text-emerald-500">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      stone: colors.stone,
+      sky: colors.sky,
+      neutral: colors.neutral,
+      gray: colors.gray,
+      slate: colors.slate,
+    },
+  },
+  plugins: [],
+};
